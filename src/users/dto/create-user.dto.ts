6fef92ca1dd422962a1f0703d338a3aeb12c66ab
akhil-haridas <4khilharidas@gmail.com>
2023-05-23 21:27:14 +0530

    user service cleanup

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,5 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+    id: number
+    name: string
+    place:string
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,23 +4,30 @@ import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
+
+  private users = [{ id: 1, name: "AKHIL", place: "PALAKKAD" }]
+  
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    this.users.push(createUserDto)
+    return `This action adds a new user`;
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.users;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.users.find(user=> user.id === id)
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
+    let objINDEX = this.users.findIndex((user) => user.id === id)
+    this.users[objINDEX] = updateUserDto
     return `This action updates a #${id} user`;
   }
 
   remove(id: number) {
+    this.users = this.users.filter((user)=> user.id !== id)
     return `This action removes a #${id} user`;
   }
 }
